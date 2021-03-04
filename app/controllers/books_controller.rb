143ed89249dd4c0e9995bd865880337b74ff8c68
mapi8808 <mapi.008.k@gmail.com>
2021-03-04 09:27:33 +0000

    [add]bootstrapの練習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,40 @@
 class BooksController < ApplicationController
+  
+  def top
+  end
+  
+  def about
+  end
+  
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
-
+  
   def edit
+    @book = Book.find(params[:id])
   end
 
-  def shw
+  def show
+    @book = Book.find(params[:id])
   end
+  
+  def create
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+  def book_params
+  params.require(:book).pirmit(:title, :body, :image_id)
+  end
+  
+    
 end
