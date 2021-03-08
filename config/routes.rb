1fb2ae0d8de5b68d8ede81df9c53be49bfb41cc3
mapi8808 <mapi.008.k@gmail.com>
2021-03-08 09:33:51 +0000

    [add]devise関係のコントローラとビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  
+  devise_for :users
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+}
+
   resources :books
   root to: 'books#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_083600) do
+ActiveRecord::Schema.define(version: 2021_03_08_092107) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_03_08_083600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
