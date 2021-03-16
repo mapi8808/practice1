9c8aacb6b8dcf58203980b230f787d04d8ecd86d
mapi8808 <mapi.008.k@gmail.com>
2021-03-16 14:30:00 +0000

    [add]ユーザー画像を投稿する準備をしています。

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -9,12 +9,15 @@ class User::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image_id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_092107) do
+ActiveRecord::Schema.define(version: 2021_03_16_142526) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_092107) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
